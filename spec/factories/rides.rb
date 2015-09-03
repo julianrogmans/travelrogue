8aa7aa18a8d32e9fc442950b021ebca2a1f5233c
Jules Rogmans <websitewizardguy@gmail.com>
2015-09-03 12:28:46 -0700

    Empty ride resource from rails generator

---
+++ spec/factories/rides.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :ride do
+    origin "Origin"
+    destination "Destination"
+    seat_count 1
+    date "2015-09-02"
+    departure_time "2015-09-02 15:44:51"
+    return_time "2015-09-02 15:44:51"
+  end
+
+end
