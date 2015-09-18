50edae284c64f6501965d7cab03aac3771e2e305
Jules Rogmans <websitewizardguy@gmail.com>
2015-09-17 18:41:16 -0700

    invalid ride spec with passing code

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,3 @@
 class Ride < ActiveRecord::Base
+  validates_presence_of :origin, :destination, :seat_count, :date, :departure_time
 end
--- spec/features/create_ride_spec.rb
+++ spec/features/create_ride_spec.rb
@@ -4,9 +4,11 @@ require "active_support"
 feature "User creates a ride" do
   let(:origin) { Faker::Address.street_address }
   let(:time) { Faker::Time.forward(2, :morning) }
+  before(:each) do
+    visit new_ride_path
+  end
 
   scenario "with valid data" do
-    visit new_ride_path
     fill_in "Origin", with: origin
     fill_in "Destination", with: Faker::Address.street_address
     fill_in "Seat count", with: 3
@@ -15,5 +17,11 @@ feature "User creates a ride" do
     fill_in "Return time", with: time + 2.hours
     click_on "Create Ride"
     expect(page).to have_content origin
+  end
+
+  scenario "with invalid data" do
+    fill_in "Origin", with: origin
+    click_on "Create Ride"
+    expect(page).to have_selector('#new_ride')
   end
 end
