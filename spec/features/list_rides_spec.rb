bd402a574ea425c86c1b3ac6d5ac2aa93c580ce4
Jules Rogmans <websitewizardguy@gmail.com>
2015-09-20 13:15:07 -0700

    Add spec context for when no rides exist

--- spec/features/list_rides_spec.rb
+++ spec/features/list_rides_spec.rb
@@ -3,25 +3,35 @@ require "rails_helper"
 feature "List rides" do
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
-  let!(:ride1) { create(:ride, user: user1) }
-  let!(:ride2) { create(:ride, user: user2) }
 
-  context "when signed in" do
-    scenario "shows rides offered by others" do
-      visit new_user_session_path
-      fill_in "Email", with: user1.email
-      fill_in "Password", with: user1.password
-      click_on "Log in"
-      expect(page).to have_content(ride2.origin)
-      expect(page).to have_no_content(ride1.origin)
+  context "when rides exist" do
+    let!(:ride1) { create(:ride, user: user1) }
+    let!(:ride2) { create(:ride, user: user2) }
+
+    context "when signed in" do
+      scenario "shows rides offered by others" do
+        visit new_user_session_path
+        fill_in "Email", with: user1.email
+        fill_in "Password", with: user1.password
+        click_on "Log in"
+        expect(page).to have_content(ride2.origin)
+        expect(page).to have_no_content(ride1.origin)
+      end
+    end
+
+    context "when not signed in" do
+      scenario "shows all rides" do
+        visit rides_path
+        expect(page).to have_content ride1.origin
+        expect(page).to have_content ride2.destination
+      end
     end
   end
 
-  context "when not signed in" do
-    scenario "shows all rides" do
+  context "when no rides exist" do
+    scenario "shows message indicating no rides" do
       visit rides_path
-      expect(page).to have_content ride1.origin
-      expect(page).to have_content ride2.destination
+      expect(page).to have_content "No rides"
     end
   end
 end
