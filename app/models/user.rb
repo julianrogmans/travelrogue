3f96ac34405d3ce1621951888caedbc022d26bdb
Jules Rogmans <websitewizardguy@gmail.com>
2015-09-26 17:06:01 -0700

    Change User#is_passenger? to loop only through passengers of ride specified

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def is_passenger?(ride_id)
-    self.passengers.inject(false) do |memo, passenger|
-      passenger.user_id == self.id ? true : memo
+    Ride.find(ride_id).passengers.inject(false) do |memo, passenger|
+      passenger.user_id == id ? true : memo
     end
   end
 end
