304f1e204f5da8a290160cce8c75d8b9d9b5ab68
Jules Rogmans <websitewizardguy@gmail.com>
2015-09-26 19:46:43 -0700

    Add example for when user is already a passenger

--- spec/features/request_ride_spec.rb
+++ spec/features/request_ride_spec.rb
@@ -32,16 +32,25 @@ feature "Request ride" do
     end
   end
 
-  context "when ride full" do
+  context "when user is already a passenger" do
+    let!(:passenger) do
+      create(:passenger, user_id: user.id, ride_id: ride.id)
+    end
+
+    scenario "cancles request with already taking ride message" do
+      visit confirm_ride_path ride.id
+      click_on "Confirm"
+      expect(page).to have_text "You are already taking this ride"
+    end
+  end
+
+  context 'when ride is full' do
     let(:ride) { create(:ride, passengers: [create(:passenger)]) }
 
-    scenario "it cancles request with error message" do
+    scenario "cancles request with ride full message" do
       visit confirm_ride_path ride.id
       click_on "Confirm"
       expect(page).to have_text "Sorry this ride is now full"
-      within "#ride_#{ride.id}" do
-        expect(page).to have_text "Ride full"
-      end
     end
   end
 end
