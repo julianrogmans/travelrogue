b1c00eda7bddb84b90dc30179f07e0beff23a774
Jules Rogmans <websitewizardguy@gmail.com>
2015-10-06 20:39:22 -0700

    Add age field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20150922001416) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
+    t.integer  "age",                                 null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
